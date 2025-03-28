3424b37fb0f95b034102677111eaa12cc1ef0aae
Nelson Tsa <t.nelson@hotmail.fr>
2025-03-28 09:40:45 +0100

    recupération de la ville latitude et longitude

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// A toi de jouer pour cette partie :-) Happy coding !
+const inputVille = document.querySelector("#cityInput");
+const bouton = document.getElementById("submitButton");
+
+
+
+
+bouton.addEventListener("click", function (event) {
+    event.preventDefault();
+    const ville = inputVille.value;
+    fetchCoordinates(ville);
+});
+
+
+
+async function fetchCoordinates(ville) {
+    const response = await fetch(
+        `https://nominatim.openstreetmap.org/search?q=${ville}&format=json&addressdetails=1&limit=1&polygon_svg=1&extratags=1&namedetails=1&countrycodes=fr&accept-language=fr&bounded=1`
+    );
+   
+    const data = await response.json();
+    console.log(data);
+    latitude = data[0].lat;
+    longitude = data[0].lon;
+    console.log(latitude, longitude);
+  
+    return latitude, longitude;
+}
+
