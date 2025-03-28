e2cc212bf62e7f3811b2c3ee7427454e1736e4d2
Nelson Tsa <t.nelson@hotmail.fr>
2025-03-28 09:48:35 +0100

    affichage de la ville et de sa position, gestion si la ville n'existe pas

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const inputVille = document.querySelector("#cityInput");
-const bouton = document.getElementById("submitButton");
+const bouton = document.getElementById("searchButton");
+const temperature = document.querySelector("#temperature");
+const city = document.querySelector("#city");
+const details = document.querySelector("#details");
+const gps = document.querySelector("#gps");
+
+
+let latitude;
+let longitude;
 
 
 
@@ -19,10 +27,30 @@ async function fetchCoordinates(ville) {
    
     const data = await response.json();
     console.log(data);
+    if (data === undefined || data.length === 0) {
+        alert("Ville introuvable !");
+        return;
+    }
     latitude = data[0].lat;
     longitude = data[0].lon;
     console.log(latitude, longitude);
-  
+    city.innerHTML = ville;
+    gps.innerHTML = `${latitude}, ${longitude}`;
+    fetchWeather(latitude, longitude).then((data) => {
+        console.log(data);
+    });
     return latitude, longitude;
 }
 
+async function fetchWeather(latitude, longitude) {
+    const response = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=sunrise,daylight_duration,sunshine_duration,sunset,weather_code,apparent_temperature_max,apparent_temperature_min,uv_index_max,rain_sum,showers_sum,snowfall_sum,precipitation_sum,precipitation_hours,precipitation_probability_max,wind_direction_10m_dominant,wind_speed_10m_max&hourly=temperature_2m,relative_humidity_2m,rain,snowfall,wind_speed_10m,weather_code,visibility,uv_index&current=temperature_2m,relative_humidity_2m,is_day,precipitation,rain,showers,snowfall,weather_code,wind_speed_10m,wind_direction_10m,apparent_temperature&timezone=auto`
+    );
+    const data = await response.json();
+
+
+    console.log(data);
+  
+   
+    return data;
+}
